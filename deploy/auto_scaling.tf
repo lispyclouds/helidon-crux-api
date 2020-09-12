00c1d91e5a222f4d68f7067c44d85135f913700b
Rahul De <rahul080327@gmail.com>
2020-09-12 15:24:58 +0200

    clean up auto scaling

--- deploy/auto_scaling.tf
+++ deploy/auto_scaling.tf
@@ -1,5 +1,3 @@
-# auto_scaling.tf
-
 resource "aws_appautoscaling_target" "target" {
   service_namespace  = "ecs"
   resource_id        = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.main.name}"
@@ -51,44 +49,3 @@ resource "aws_appautoscaling_policy" "down" {
   depends_on = [
   aws_appautoscaling_target.target]
 }
-
-# CloudWatch alarm that triggers the autoscaling up policy
-resource "aws_cloudwatch_metric_alarm" "service_cpu_high" {
-  alarm_name          = "hc_cpu_utilization_high"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/ECS"
-  period              = "60"
-  statistic           = "Average"
-  threshold           = "85"
-
-  dimensions = {
-    ClusterName = aws_ecs_cluster.main.name
-    ServiceName = aws_ecs_service.main.name
-  }
-
-  alarm_actions = [
-  aws_appautoscaling_policy.up.arn]
-}
-
-# CloudWatch alarm that triggers the autoscaling down policy
-resource "aws_cloudwatch_metric_alarm" "service_cpu_low" {
-  alarm_name          = "hc_cpu_utilization_low"
-  comparison_operator = "LessThanOrEqualToThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/ECS"
-  period              = "60"
-  statistic           = "Average"
-  threshold           = "10"
-
-  dimensions = {
-    ClusterName = aws_ecs_cluster.main.name
-    ServiceName = aws_ecs_service.main.name
-  }
-
-  alarm_actions = [
-  aws_appautoscaling_policy.down.arn]
-}
-
